48e70f60b9fd068ecba94bdefc41289c2709ed76
JANew98 <jasonnewton1998@gmail.com>
2023-02-05 15:54:47 +0000

    quick fix

--- queries.tf
+++ queries.tf
@@ -1,4 +1,6 @@
 data "aws_key_pair" "test_key" {
+    name = "terraform_key"
+    
     filter {
       name = "tag:targets"
       values = [ "bastion" ]
@@ -6,6 +8,8 @@ data "aws_key_pair" "test_key" {
 }
 
 data "aws_key_pair" "test_key_1" {
+    name = "ansikey"
+    
     filter {
       name = "tag:targets"
       values = [ "nodes" ]
